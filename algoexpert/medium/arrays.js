7fe6715c45771211b35d68f7c0ce407ba38cd81b
hiendbui <hiendbui0@gmail.com>
2021-02-13 16:42:55 -0800

    Add spiral traverse

--- algoexpert/medium/arrays.js
+++ algoexpert/medium/arrays.js
@@ -82,3 +82,38 @@ function isMonotonic(array) {
 	}
 	return true
 }
+
+//O(m*n) T | O(m*n) S
+function spiralTraverse(array) {
+	let startRow = 0;
+	let endRow = array.length-1;
+	let startCol = 0;
+	let endCol = array[0].length-1;
+	let newArr = [];
+	
+	while (startRow <= endRow && startCol <= endCol) {
+		for (let i = startCol; i <= endCol; i++) {
+			newArr.push(array[startRow][i])
+		}
+		
+		for (let i = startRow+1; i <= endRow; i++) {
+			newArr.push(array[i][endCol]);
+		}
+		
+		for (let i = endCol-1; i >= startCol; i--) {
+			if (startRow === endRow) break;
+			newArr.push(array[endRow][i]);
+		}
+		
+		for (let i = endRow-1; i > startRow; i--) {
+			if (startCol === endCol) break;
+			newArr.push(array[i][startCol]);
+		}
+		
+		startRow++;
+		endRow--;
+		startCol++;
+		endCol--
+	}
+	return newArr
+ }
