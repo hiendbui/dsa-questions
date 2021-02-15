a712e2367ed20f3c148d96bd512015e405febb2d
hiendbui <hiendbui0@gmail.com>
2021-02-15 15:18:53 -0800

    Add BFS

---
+++ algoexpert/medium/graphs.js
@@ -0,0 +1,24 @@
+// BFS O(v+e)T | O(v) S
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  breadthFirstSearch(array) {
+    let queue = [this];
+		while (queue.length > 0) {
+			let node = queue.shift();
+			array.push(node.name);
+			node.children.forEach((child) => {
+				queue.push(child);
+			})
+		}
+		return array;
+  }
+}
