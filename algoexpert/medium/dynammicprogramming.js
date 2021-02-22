a0ce741988a621c041746ccc77b6597673130fb2
hiendbui <hiendbui0@gmail.com>
2021-02-21 23:57:00 -0800

    Add min coins for change

--- algoexpert/medium/dynammicprogramming.js
+++ algoexpert/medium/dynammicprogramming.js
@@ -26,3 +26,18 @@ function numberOfWaysToMakeChange(n, denoms) {
 	})
 	return ways[n]
 }
+
+//O(nd) T | O(n) S
+function minNumberOfCoinsForChange(n, denoms) {
+  const arr = new Array(n+1).fill(Infinity);
+	arr[0] = 0;
+	denoms.forEach((denom) => {
+		for (let amount = 1; amount < arr.length; amount++) {
+			if (denom <= amount) {
+				arr[amount] = Math.min(arr[amount],arr[amount - denom]+1);
+			}
+		}
+	})
+	
+	return arr[n] !== Infinity ? arr[n] : -1;
+}
