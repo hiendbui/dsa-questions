c9ab0511328bdab395ab0a97333d188f18301d30
hiendbui <hiendbui0@gmail.com>
2021-02-28 21:26:18 -0800

    Add letter combos

--- leetcode/medium/strings.js
+++ leetcode/medium/strings.js
@@ -21,3 +21,47 @@ const minCost = function(s, cost) {
     
     return deletionCost;
 };
+
+
+
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter 
+// combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given below. 
+// Note that 1 does not map to any letters.
+
+const letterCombinations = function(digits) {
+    if (digits.length === 0) return [];
+    
+    const dict = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
+    
+    let resArr = [];
+    
+    for (let i=0; i<dict[digits[0]].length; i++) {
+        resArr.push(dict[digits[0]][i]);
+    }
+    
+    for (let i=1; i<digits.length; i++) {
+        const newArr = [];
+        resArr.forEach((str)=>{
+           let j = 0;
+            while (j < dict[digits[i]].length) {
+                newArr.push(str + dict[digits[i]][j]);
+                j++;
+            }
+        });
+        resArr = newArr;
+    }
+    
+    return resArr;
+};
