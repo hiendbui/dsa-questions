ea1b1c0014abcdf8496c5394169ecebc9bccf155
hiendbui <hiendbui0@gmail.com>
2021-03-01 17:43:46 -0800

    Add description

--- leetcode/medium/strings.js
+++ leetcode/medium/strings.js
@@ -70,7 +70,10 @@ const letterCombinations = function(digits) {
 
 
 
+// Given a string S, check if the letters can be rearranged so that two characters 
+// that are adjacent to each other are not the same.
 
+// If possible, output any possible result.  If not possible, return the empty string.
 
 const reorganizeString = function(S) {
     const charCount = new Map();
