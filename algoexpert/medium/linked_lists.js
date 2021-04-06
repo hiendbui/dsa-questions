f49f6115340231e63839df405e143922514d2f82
hiendbui <hiendbui0@gmail.com>
2021-04-05 20:05:12 -0700

    Add sum of linked lists

--- algoexpert/medium/linked_lists.js
+++ algoexpert/medium/linked_lists.js
@@ -22,3 +22,39 @@ function removeKthNodeFromEnd(head, k) {
 	
 	curNode.next = curNode.next.next;
 }
+
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+
+//sum of linked lists
+function sumOfLinkedLists(linkedListOne, linkedListTwo) {
+    const sum = calcNumber(linkedListOne) + calcNumber(linkedListTwo);
+	const sumArr = sum.toString().split('');
+	sumArr.forEach((numStr,i) => sumArr[i] = parseInt(numStr));
+	
+	const sumLinkedList = new LinkedList(sumArr[sumArr.length-1]);
+	let curNode = sumLinkedList;
+	for (let i = sumArr.length-2; i>=0; i--) {
+		curNode.next = new LinkedList(sumArr[i]);
+		curNode = curNode.next;
+	}
+	
+	return sumLinkedList;
+}
+
+function calcNumber(linkedList) {
+	const arr = [];
+	let curNode = linkedList;
+	
+	while (curNode) {
+		arr.push(curNode.value.toString());
+		curNode = curNode.next;
+	}
+	
+	return parseInt(arr.reverse().join(''));
+}
