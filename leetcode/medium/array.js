1c4d302f5930dbc4ea937a1c5320c9072111b629
hiendbui <hiendbui0@gmail.com>
2021-04-10 22:11:41 -0700

    Add t/s complexity

--- leetcode/medium/array.js
+++ leetcode/medium/array.js
@@ -127,6 +127,8 @@ const groupAnagrams = function(strs) {
 // Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 // Output: [[1,6],[8,10],[15,18]]
 // Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+//O(nlogn) T | O(n) S 
 var merge = function(intervals) {
     intervals.sort((a,b)=>a[0]-b[0]);
     
