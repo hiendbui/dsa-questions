ee74361b6974591de22dec51dc3d95fd7af88931
hiendbui <hiendbui0@gmail.com>
2021-04-10 22:10:04 -0700

    Add merged intervals

--- leetcode/medium/array.js
+++ leetcode/medium/array.js
@@ -114,3 +114,35 @@ const groupAnagrams = function(strs) {
     
     return Object.values(anagrams);
 };
+
+
+// Given an array of intervals where intervals[i] = [starti, endi], merge all 
+// overlapping intervals, and return an array of the non-overlapping intervals that 
+// cover all the intervals in the input.
+
+ 
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+var merge = function(intervals) {
+    intervals.sort((a,b)=>a[0]-b[0]);
+    
+    const mergedIntervals = [];
+    let curInterval = intervals[0];
+    
+    for (let i=0; i<intervals.length; i++) {
+        if (intervals[i][0] <= curInterval[1]) {
+            curInterval[1] = Math.max(curInterval[1], intervals[i][1]);
+        } else {
+            mergedIntervals.push(curInterval);
+            curInterval = intervals[i];
+        }
+    }
+    
+    mergedIntervals.push(curInterval);
+    
+    return mergedIntervals;
+};
