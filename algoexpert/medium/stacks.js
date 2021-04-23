c4ea3e96b35841d0ec67478bcf7560164a40468b
hiendbui <hiendbui0@gmail.com>
2021-04-23 15:33:19 -0700

    add sunset views

--- algoexpert/medium/stacks.js
+++ algoexpert/medium/stacks.js
@@ -22,3 +22,25 @@ function balancedBrackets(string) {
 	
 	return stack.length === 0;
 }
+
+
+//O(n) T | O(n) S
+function sunsetViews(buildings, direction) {
+	if (buildings.length === 0) return [];
+  	const sunsetBuildings = [];
+  	const startIdx = direction === "WEST" ? 0 : buildings.length-1;
+	const step = direction === "WEST" ? 1 : -1; 
+	
+	let tallest = -1;
+	let i = startIdx;
+	while (i >= 0 && i < buildings.length) {
+		const building = buildings[i]
+		if (building > tallest) sunsetBuildings.push(i);
+		tallest = Math.max(tallest,building);
+		i = i + step;
+	}
+
+	
+	if (direction === 'EAST') sunsetBuildings.reverse();
+	return sunsetBuildings;
+}
